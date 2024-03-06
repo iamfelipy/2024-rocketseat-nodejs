abf46906d3b7236b0a6e0d353982a9236a49a74c
felipy <felipy81br@gmail.com>
2024-03-06 20:50:57 -0300

    typescript com tsx watch mais script npm

---
+++ 2-rotas-e-http/src/server.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fastify_1 = require("fastify");
+var app = (0, fastify_1.default)();
+// GET, POST, PUT, PATCH, DELETE
+app.get('/hello', function () {
+    return 'Hello World';
+});
+app.listen({
+    port: 3333
+}).then(function () {
+    console.log('HTTP Server Running!');
+});
