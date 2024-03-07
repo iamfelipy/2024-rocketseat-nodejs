3d4c9048ea0ffc20bb07ff183473c814b8e5291b
felipy <felipy81br@gmail.com>
2024-03-07 08:30:45 -0300

    configuração do eslint -> padronização da formatação do código em equipe

--- 2-rotas-e-http/src/server.ts
+++ 2-rotas-e-http/src/server.ts
@@ -5,11 +5,13 @@ const app = fastify()
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
-app.listen({
-    port: 3333
-}).then(()=>{
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-})
+  })
