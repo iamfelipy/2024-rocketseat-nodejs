5076854de4a3967f0968bc5986ff6b057b258574
felipy <felipy81br@gmail.com>
2024-03-13 23:11:57 -0300

    1-configurando NODE_ENV para teste 2-finalização dos testes 3-build com a tsup

--- 2-rotas-e-http/src/test/test.spec.ts
+++ 2-rotas-e-http/src/test/test.spec.ts
@@ -1,35 +1,154 @@
-import { test, expect, beforeAll, afterAll } from 'vitest'
+import {
+  test,
+  it,
+  expect,
+  beforeAll,
+  afterAll,
+  describe,
+  beforeEach,
+} from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
+import { execSync } from 'child_process'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    // aguardar todos os plugins seem cadastrados
+    await app.ready()
+  })
 
-afterAll(async () => {
-  // fechar a aplicação, remover da memoria
-  await app.close()
-})
+  afterAll(async () => {
+    // fechar a aplicação, remover da memoria
+    await app.close()
+  })
 
-test('User can create a new trasaction', async () => {
-  // forma 1 de executar
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
 
-  // const response = await request(app.server).post('transactions').send({
-  //   title: 'New transaction',
-  //   amount: 5000,
-  //   type: 'credit',
-  // })
+  // alternativa ao test
+  // escrita como se estivesse escrevendo requisitos funcionais
+  it('should be able to create a new trasaction', async () => {
+    // test('User can create a new trasaction', async () => {
+    // forma 1 de executar
 
-  // expect(response.statusCode).toEqual(201)
+    // const response = await request(app.server).post('/transactions').send({
+    //   title: 'New transaction',
+    //   amount: 5000,
+    //   type: 'credit',
+    // })
 
-  // forma 2 de executar
+    // expect(response.statusCode).toEqual(201)
 
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
+    // forma 2 de executar
+
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      // sintaxe exclusiva do super test
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    //   //forma 1 de validar objetos de array
+    //   expect(listTransactionsResponse.body).toEqual([
+    //   {
+    //     id: expect.any(String),
+    //   },
+    // ])
+    // forma 2 de validar objetos
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+
+  test('should be able to get a specific transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
     })
-    .expect(201)
+  })
+
+  // it.only('só quero testar esse', () => {})
+  it.skip('pula o teste atual, enquanto posso testar outros e ir desenvolvedo aos poucos', () => {})
+  it.todo(
+    'esse método todo é para me lembrar de um teste que devo fazer no futuro',
+  )
 })
