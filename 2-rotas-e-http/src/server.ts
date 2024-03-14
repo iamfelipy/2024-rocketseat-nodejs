795c5efa86b57f9b625f8762d067e722a7b8126c
felipy <felipy81br@gmail.com>
2024-03-14 14:16:59 -0300

    correção 1: render erro deploy

--- 2-rotas-e-http/src/server.ts
+++ 2-rotas-e-http/src/server.ts
@@ -4,6 +4,8 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    // config para funcionar o deploy render
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP Server Running!')
