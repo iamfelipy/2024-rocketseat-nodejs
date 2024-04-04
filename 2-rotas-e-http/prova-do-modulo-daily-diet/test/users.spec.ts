62b6b4d86cbdf15499e2a8440cb42eb4ca4dbef0
felipy <felipy81br@gmail.com>
2024-04-03 23:30:37 -0300

    1-mudar test user.spec 2-atualizar .gitignore

--- 2-rotas-e-http/prova-do-modulo-daily-diet/test/users.spec.ts
+++ 2-rotas-e-http/prova-do-modulo-daily-diet/test/users.spec.ts
@@ -1,9 +1,9 @@
 import supertest from 'supertest'
 import { execSync } from 'node:child_process'
-import { beforeEach, describe, it, beforeAll, afterAll } from 'vitest'
-import { app } from '../app'
+import { beforeEach, describe, it, beforeAll, afterAll, expect } from 'vitest'
+import { app } from '../src/app'
 
-describe("User's routes", () => {
+describe('Users routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -18,12 +18,24 @@ describe("User's routes", () => {
   })
 
   it('should be able to create a new user', async () => {
-    await supertest(app.server)
+    const response = await supertest(app.server)
       .post('/users')
       .send({
         name: 'felcam',
         email: 'felcam@gmail.com',
       })
       .expect(201)
+
+    const cookies = response.get('Set-Cookie')
+
+    expect(cookies).toEqual(
+      expect.arrayContaining([expect.stringContaining('sessionId')]),
+    )
+    /*
+      linha 32 testa isso:
+      [
+        'sessionId=456f8668-88c9-490a-9ae3-4ccdd7deab68; Max-Age=604800; Path=/'
+      ]
+    */
   })
 })
