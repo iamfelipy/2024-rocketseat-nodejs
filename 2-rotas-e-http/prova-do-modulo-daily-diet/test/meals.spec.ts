5f278c89bc533b7d3442f6f208d47cfe1fa87f26
felipy <felipy81br@gmail.com>
2024-04-04 16:26:59 -0300

    teste implementados: 1-should be able to update a meal from a user 2-should be able to delete a meal from a user 3-shoud be able to get metrics from a user

--- 2-rotas-e-http/prova-do-modulo-daily-diet/test/meals.spec.ts
+++ 2-rotas-e-http/prova-do-modulo-daily-diet/test/meals.spec.ts
@@ -125,4 +125,168 @@ describe('Meals routes', () => {
       }),
     })
   })
+  it('should be able to update a meal from a user', async () => {
+    const createUserResponse = await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'paulo',
+        email: 'paulo@gmail.com',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')!
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: 'Apple, milk and cookies',
+        isOnDiet: false,
+        date: Date.now() + 60 * 60,
+      })
+      .expect(201)
+
+    const listMealsResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const mealId = listMealsResponse.body.meals[0].id
+
+    await supertest(app.server)
+      .put(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast updated',
+        description: 'bread, mean and rice',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(204)
+
+    // const mealResponse = await supertest(app.server)
+    //   .get(`/meals/${mealId}`)
+    //   .set('Cookie', cookies)
+    //   .expect(200)
+
+    // expect(mealResponse.body).toEqual({
+    //   meal: expect.objectContaining({
+    //     name: 'Breakfast updated',
+    //     description: 'bread, mean and rice',
+    //     is_on_diet: 1,
+    //     date: expect.any(Number),
+    //   }),
+    // })
+  })
+  it('should be able to delete a meal from a user', async () => {
+    const userResponse = await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'bruno',
+        email: 'bruno@gmail.com',
+      })
+      .expect(201)
+
+    const cookies = userResponse.get('Set-Cookie')!
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Dinner',
+        description: 'beans, rice and means',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    const listMealsResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const mealId = listMealsResponse.body.meals[0].id
+
+    await supertest(app.server)
+      .delete(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .expect(204)
+  })
+  it.only('shoud be able to get metrics from a user', async () => {
+    const createUserResponse = await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'João',
+        email: 'joao@gmail.com',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')!
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date('2021-01-01T08:00:00'),
+      })
+      .expect(201)
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Lunch',
+        description: "It's a lunch",
+        isOnDiet: false,
+        date: new Date('2021-01-01T12:00:00'),
+      })
+      .expect(201)
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Snack',
+        description: "It's a snack",
+        isOnDiet: true,
+        date: new Date('2021-01-01T15:00:00'),
+      })
+      .expect(201)
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Dinner',
+        description: "It's a dinner",
+        isOnDiet: true,
+        date: new Date('2021-01-01T20:00:00'),
+      })
+      .expect(201)
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date('2021-01-02T08:00:00'),
+      })
+      .expect(201)
+
+    const getMetricsResponse = await supertest(app.server)
+      .get('/meals/metrics')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getMetricsResponse.body).toEqual(
+      expect.objectContaining({
+        totalMeals: 5,
+        totalMealsOnDiet: 4,
+        totalMealsOffDiet: 1,
+        bestOnDietSequence: 3,
+      }),
+    )
+  })
 })
