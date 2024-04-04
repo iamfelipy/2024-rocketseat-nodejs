b1c69198efd5210b849b11a4a089fc9edadab1fe
felipy <felipy81br@gmail.com>
2024-04-03 23:56:08 -0300

    1-test meals route: deve ser póssivel ver uma unica refeição

--- 2-rotas-e-http/prova-do-modulo-daily-diet/test/meals.spec.ts
+++ 2-rotas-e-http/prova-do-modulo-daily-diet/test/meals.spec.ts
@@ -82,4 +82,47 @@ describe('Meals routes', () => {
     expect(listMealsResponse.body.meals[0].name).toBe('Breakfast')
     expect(listMealsResponse.body.meals[1].name).toBe('Lunch')
   })
+  it('should be able to show a single meal', async () => {
+    const createUserResponse = await supertest(app.server)
+      .post('/users')
+      .send({
+        name: 'jamaica',
+        email: 'jamaica@gmail.com',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')!
+
+    await supertest(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date(Date.now() * 24 * 60 * 60 * 1000), // 1 day after
+      })
+      .expect(201)
+
+    const listMealsResponse = await supertest(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const mealId = listMealsResponse.body.meals[0].id
+
+    const mealResponse = await supertest(app.server)
+      .get(`/meals/${mealId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(mealResponse.body).toEqual({
+      meal: expect.objectContaining({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        is_on_diet: 1,
+        date: expect.any(Number),
+      }),
+    })
+  })
 })
