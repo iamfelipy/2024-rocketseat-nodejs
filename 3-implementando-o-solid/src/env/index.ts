b10dee2a49b805fca1ecc37bc30b546606829c28
felipy <felipy81br@gmail.com>
2024-04-08 17:15:49 -0300

    configurações: 1-eslint rocketseat-node 2-.eslintignore 3--alias para importação rapida no tsconfig.json

--- 3-implementando-o-solid/src/env/index.ts
+++ 3-implementando-o-solid/src/env/index.ts
@@ -1,5 +1,5 @@
 // import 'dotenv/config' cexecuta tudo dentro do arquivo config
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
 // process.env: { NODE_ENV: 'dev', ... }
@@ -8,12 +8,12 @@ config()
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.error('Invalid environment variable', _env.error.format())
 
   throw new Error('Invalid environment variables.')
--- 3-implementando-o-solid/src/server.ts
+++ 3-implementando-o-solid/src/server.ts
@@ -1,10 +1,12 @@
-import { app } from "./app";
+import { app } from './app'
 import { env } from './env'
 
-app.listen({
-  // configuração obrigatória para fazer o deploy
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    // configuração obrigatória para fazer o deploy
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
