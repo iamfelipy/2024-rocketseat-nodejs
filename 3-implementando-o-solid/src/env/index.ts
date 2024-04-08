0df804e4f457d4f92dd7a7572a632267c1aa7988
felipy <felipy81br@gmail.com>
2024-04-08 15:49:45 -0300

    Configuração: .ENV com ZOD

---
+++ 3-implementando-o-solid/src/env/index.ts
@@ -0,0 +1,22 @@
+// import 'dotenv/config' cexecuta tudo dentro do arquivo config
+import {config} from 'dotenv'
+import { z } from 'zod'
+
+// process.env: { NODE_ENV: 'dev', ... }
+
+config()
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false) {
+  console.error('Invalid environment variable', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
