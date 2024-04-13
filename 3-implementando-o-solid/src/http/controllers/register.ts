62a50181e26e8a78704e5fe6fb92db837bf3ccef
felipy <felipy81br@gmail.com>
2024-04-13 14:27:12 -0300

    1-criar o plugin de rotas 2-criar o controller register 3-hash da senha com bcyptjs

---
+++ 3-implementando-o-solid/src/http/controllers/register.ts
@@ -0,0 +1,42 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  /*
+    a duas formas de transforma a senha em hash com bcryptjs:
+    1-round:
+      quanto maior a quantidade de rounds, maior o processamento
+    2-uma senha no env
+  */
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+
+  return reply.status(201).send()
+}
