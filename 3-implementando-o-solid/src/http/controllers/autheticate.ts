12394ab0c82f7e1699f03eaa903a23b56265e30b
felipy <felipy81br@gmail.com>
2024-04-19 12:06:31 -0300

    criação da rota e do controller authenticate

---
+++ 3-implementando-o-solid/src/http/controllers/autheticate.ts
@@ -0,0 +1,35 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const autheticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { email, password } = autheticateBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const autheticateUseCase = new AuthenticateUseCase(usersRepository)
+
+    await autheticateUseCase.execute({
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(200).send()
+}
