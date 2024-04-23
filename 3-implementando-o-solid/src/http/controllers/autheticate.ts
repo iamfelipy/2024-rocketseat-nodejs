382e630f37315dd116a660e51647e0b70a5a16f0
felipy <felipy81br@gmail.com>
2024-04-22 22:00:51 -0300

    aplicando factory pattern para o authenticate

--- 3-implementando-o-solid/src/http/controllers/autheticate.ts
+++ 3-implementando-o-solid/src/http/controllers/autheticate.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -16,8 +15,7 @@ export async function authenticate(
   const { email, password } = autheticateBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const autheticateUseCase = new AuthenticateUseCase(usersRepository)
+    const autheticateUseCase = makeAuthenticateUseCase()
 
     await autheticateUseCase.execute({
       email,
