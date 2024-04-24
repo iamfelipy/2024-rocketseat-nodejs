60f40d1e4ae8224d2e4cf6a4208563eb6d5ce8a7
felipy <felipy81br@gmail.com>
2024-04-24 15:13:51 -0300

    implementação parcial, da regra de negocio "o usuário não pode fazer 2 check-ins no mesmo dia"

--- 3-implementando-o-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 3-implementando-o-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,9 +1,33 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    // 2023-02-28T15:30
+    // 2023-02-28T00:00:00
+    // essa função com a opção date vai retornar apartir da meia noite que é o começo do dia
+    const startOfTheDay = dayjs(date).startOf('date')
+    // retorna a data com o ultimo horario valido
+    // // 2023-02-28T23:59:59
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+    return checkInOnSameDate
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
--- 3-implementando-o-solid/src/use-cases/check-in.spec.ts
+++ 3-implementando-o-solid/src/use-cases/check-in.spec.ts
@@ -1,6 +1,6 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: CheckInUseCase
@@ -9,9 +9,53 @@ describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    // libero a funcionalidade de definir uma data especifica para usar nos testes
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    // volta ao estado original
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+    // defino a data especifica aqui
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
