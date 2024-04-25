5b863d43c1e6940696bf5d9f9650a9bcc0333dbd
felipy <felipy81br@gmail.com>
2024-04-25 15:49:41 -0300

    implementações "Deve ser possível cadastrar uma academia": 1-Foi criado a persistencia com InMemoryDatabase 2-Foi criado o contrato para o repository in-memory 3-Criação do use case create-gym 4-Criação do teste unitario para o create-gym

---
+++ 3-implementando-o-solid/src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+import { beforeEach, describe, it, expect } from 'vitest'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Trampolim Academia',
+      description: null,
+      phone: null,
+      latitude: -26.99875078708264,
+      longitude: -48.63171675406097,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
