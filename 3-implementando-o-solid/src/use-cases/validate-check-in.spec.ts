5369406f2a105f1d3d235dcdc0e43809c70a0913
felipy <felipy81br@gmail.com>
2025-02-13 08:34:11 -0300

    feat: prevent check-in validation after 20 minutes of creation

--- 3-implementando-o-solid/src/use-cases/validate-check-in.spec.ts
+++ 3-implementando-o-solid/src/use-cases/validate-check-in.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-c
 import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -45,5 +46,26 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    expect(() =>
+      sut.execute({
+        checkInId: checkIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
