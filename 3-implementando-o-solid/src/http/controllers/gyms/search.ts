b7afd3a34a6cf07a74b10bc80971717d892e8d99
felipy <felipy81br@gmail.com>
2025-02-20 22:15:54 -0300

    **Commit:** `feat: create HTTP controller to fetch gyms`

---
+++ 3-implementando-o-solid/src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsBodySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsBodySchema.parse(request.body)
+
+  const createGymUseCase = makeSearchGymsUseCase()
+  const gyms = await createGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
