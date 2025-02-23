1ffc03fb443971fb823dbbc0dd9287f53cd1c041
felipy <felipy81br@gmail.com>
2025-02-22 22:52:18 -0300

    test: create end-to-end test for gym creation

---
+++ 3-implementando-o-solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,29 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able ro create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gymResponse = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -26.99875078708264,
+        longitude: -48.63171675406097,
+      })
+
+    expect(gymResponse.statusCode).toEqual(201)
+  })
+})
