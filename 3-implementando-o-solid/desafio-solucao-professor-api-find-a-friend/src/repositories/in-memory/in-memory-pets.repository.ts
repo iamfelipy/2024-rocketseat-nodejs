430bae38837020446833739157c6553e69b19c4a
felipy <felipy81br@gmail.com>
2025-03-20 11:04:01 -0300

    feat: implement SearchPets use case with filtering by city, age, size, energy level, and environment

--- 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/repositories/in-memory/in-memory-pets.repository.ts
+++ 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/repositories/in-memory/in-memory-pets.repository.ts
@@ -1,7 +1,33 @@
 import { Pet, Prisma } from '@prisma/client'
-import { PetsRepository } from '../pets.repository'
+import { FindAllParams, PetsRepository } from '../pets.repository'
+import { InMemoryOrgsRepository } from './in-memory-orgs.repository'
 
 export class InMemoryPetsRepository implements PetsRepository {
+  constructor(private orgsRepository: InMemoryOrgsRepository) {}
+
+  async findAll(params: FindAllParams): Promise<Pet[]> {
+    const orgsByCity = this.orgsRepository.items.filter(
+      (org) => org.city === params.city,
+    )
+
+    const pets = this.items
+      .filter((item) => orgsByCity.some((org) => org.id === item.org_id))
+      .filter((item) => (params.age ? item.age === params.age : true))
+      .filter((item) => (params.size ? item.size === params.size : true))
+      .filter((item) =>
+        params.energy_level ? item.energy_level === params.energy_level : true,
+      )
+      .filter((item) =>
+        params.environment ? item.environment === params.environment : true,
+      )
+
+    return pets
+  }
+
+  async findById(id: string): Promise<Pet | null> {
+    return this.items.find((item) => item.id === id) ?? null
+  }
+
   public items: Pet[] = []
   async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
     const pet = {
@@ -12,9 +38,5 @@ export class InMemoryPetsRepository implements PetsRepository {
     this.items.push(pet)
 
     return pet
-  }
-
-  async findById(id: string): Promise<Pet | null> {
-    return this.items.find((item) => item.id === id) ?? null
   }
 }
---
+++ 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/use-cases/search-pets.use-case.spec.ts
@@ -0,0 +1,64 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs.repository'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets.repository'
+import { SearchPetsUseCase } from './search-pets.use-case'
+import { makeOrg } from 'test/factories/make-org.factory'
+import { makePet } from 'test/factories/make-pet.factory'
+
+describe('Search Pets Use Case', () => {
+  let orgsRepository: InMemoryOrgsRepository
+  let petsRepository: InMemoryPetsRepository
+  let sut: SearchPetsUseCase
+
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    petsRepository = new InMemoryPetsRepository(orgsRepository)
+    sut = new SearchPetsUseCase(petsRepository)
+  })
+
+  it('should be able to search pets by city', async () => {
+    const org = await orgsRepository.create(makeOrg())
+
+    await petsRepository.create(makePet({ org_id: org.id }))
+    await petsRepository.create(makePet({ org_id: org.id }))
+
+    const org2 = await orgsRepository.create(makeOrg())
+
+    await petsRepository.create(makePet({ org_id: org2.id }))
+
+    const { pets } = await sut.execute({ city: org.city })
+
+    expect(pets).toHaveLength(2)
+
+    const { pets: pets2 } = await sut.execute({ city: org2.city })
+
+    expect(pets2).toHaveLength(1)
+  })
+  it('should be able to search pets by city and size', async () => {
+    const org = await orgsRepository.create(makeOrg())
+
+    await petsRepository.create(makePet({ org_id: org.id, size: 'small' }))
+    await petsRepository.create(makePet({ org_id: org.id, size: 'medium' }))
+    await petsRepository.create(makePet({ org_id: org.id, size: 'large' }))
+
+    const { pets } = await sut.execute({ city: org.city, size: 'small' })
+
+    expect(pets).toHaveLength(1)
+  })
+  it('should be able to search pets by city and environment', async () => {
+    const org = await orgsRepository.create(makeOrg())
+
+    await petsRepository.create(
+      makePet({ org_id: org.id, environment: 'indoor' }),
+    )
+    await petsRepository.create(
+      makePet({ org_id: org.id, environment: 'outdoor' }),
+    )
+
+    const { pets } = await sut.execute({
+      city: org.city,
+      environment: 'indoor',
+    })
+
+    expect(pets).toHaveLength(1)
+  })
+})
