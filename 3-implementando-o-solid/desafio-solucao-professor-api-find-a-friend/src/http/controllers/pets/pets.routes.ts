1bfc3eec27edd09596e0fbd6da7c3a46d109fa29
felipy <felipy81br@gmail.com>
2025-03-22 13:59:16 -0300

    feat: implement get pet functionality with error handling and validation

--- 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/http/controllers/pets/pets.routes.ts
+++ 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/http/controllers/pets/pets.routes.ts
@@ -2,7 +2,9 @@ import { FastifyInstance } from 'fastify'
 
 import { createPetController } from '@/http/controllers/pets/create-pet.controller'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { getPetController } from './get-pet.controller'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', { onRequest: [verifyJwt] }, createPetController)
+  app.get('/orgs/pets/:id', getPetController)
 }
