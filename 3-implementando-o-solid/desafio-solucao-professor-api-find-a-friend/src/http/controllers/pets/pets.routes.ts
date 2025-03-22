beecee0348cc598683d69b45881de053a2a8bc19
felipy <felipy81br@gmail.com>
2025-03-22 19:50:50 -0300

    feat: implement search pets functionality with query validation

--- 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/http/controllers/pets/pets.routes.ts
+++ 3-implementando-o-solid/desafio-solucao-professor-api-find-a-friend/src/http/controllers/pets/pets.routes.ts
@@ -3,8 +3,10 @@ import { FastifyInstance } from 'fastify'
 import { createPetController } from '@/http/controllers/pets/create-pet.controller'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { getPetController } from './get-pet.controller'
+import { searchPetsController } from './search-pet.controller'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', { onRequest: [verifyJwt] }, createPetController)
+  app.get('/orgs/pets', searchPetsController)
   app.get('/orgs/pets/:id', getPetController)
 }
