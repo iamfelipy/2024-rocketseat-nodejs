4be1fff1ebc57e50ce04ab529241e039ef87687c
felipy <felipy81br@gmail.com>
2025-03-29 13:51:45 -0300

    feat: implement in-memory repositories for answers and questions, and update use case tests

--- 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/answer-question.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/answer-question.ts
@@ -8,19 +8,27 @@ interface AnswerQuestionUseCaseRequest {
   content: string
 }
 
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
+}
+
 export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
   async execute({
     instructorId,
     questionId,
     content,
-  }: AnswerQuestionUseCaseRequest) {
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
     })
+
     await this.answersRepository.create(answer)
-    return answer
+
+    return {
+      answer,
+    }
   }
 }
--- 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/create-question.spec.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,18 +1,24 @@
-import { QuestionsRepository } from '../repositories/questions-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
 
-const fakeQuestionRepository: QuestionsRepository = {
-  create: async (question: Question) => {},
-}
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase
 
-test('create a question', async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionRepository)
-
-  const { question } = await createQuestion.execute({
-    authorId: '1',
-    title: 'Nova pergunta',
-    content: 'Conteudo da pergunta',
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    // system under the test
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  expect(question.id).toBeTruthy()
+  it('should be able to create a question', async () => {
+    const { question } = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'Conteudo da pergunta',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id)
+  })
 })
