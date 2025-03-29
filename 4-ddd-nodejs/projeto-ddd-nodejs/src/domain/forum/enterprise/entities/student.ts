0b6b80ba4bff7607470a6aba2039495cad2ab0a3
felipy <felipy81br@gmail.com>
2025-03-29 11:56:28 -0300

    chore: refactor archives for the clean architecture

--- 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/enterprise/entities/student.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/enterprise/entities/student.ts
@@ -1,4 +1,4 @@
-import { Entity } from '@/core/entities/entity'
+import { Entity } from '@/domain/forum/enterprise/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
