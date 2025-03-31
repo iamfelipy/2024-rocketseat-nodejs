92a1a57d90a708ada9a47f903d91d78411cfbd9e
felipy <felipy81br@gmail.com>
2025-03-31 11:20:23 -0300

    feat: add use case to fetch question answers with unit test

--- 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/repositories/answers-repository.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,12 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
   findById(id: string): Promise<Answer | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<Answer[]>
   save(question: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
   create(answer: Answer): Promise<void>
--- 4-ddd-nodejs/projeto-ddd-nodejs/test/repositories/in-memory-answers-repository.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/test/repositories/in-memory-answers-repository.ts
@@ -1,8 +1,10 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
   async findById(id: string): Promise<Answer | null> {
     const answer = this.items.find((item) => item.id.toString() === id)
     if (!answer) {
@@ -10,6 +12,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     }
 
     return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer) {
