6ed25d5fbddbb8ba3bc08fa5d8e0888cedd2bb85
felipy <felipy81br@gmail.com>
2025-04-12 21:50:20 -0300

    feat: implement deletion of question attachments when deleting a question

--- 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/delete-question.spec.ts
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -3,13 +3,20 @@ import { DeleteQuestionUseCase } from './delete-question'
 import { makeQuestion } from 'test/factories/make-question'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let sut: DeleteQuestionUseCase
 
 describe('Delete Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
     // system under the test
     sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
   })
@@ -19,6 +26,18 @@ describe('Delete Question', () => {
       { authorId: new UniqueEntityID('author-1') },
       new UniqueEntityID('question-1'),
     )
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    )
+
     await inMemoryQuestionsRepository.create(newQuestion)
     await sut.execute({
       questionId: 'question-1',
@@ -26,6 +45,7 @@ describe('Delete Question', () => {
     })
 
     expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(0)
   })
 
   it('should not be able to delete a question from another user', async () => {
