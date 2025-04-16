b875292b1ca3dfe700577787ab0ec45f3d7f5f5a
felipy <felipy81br@gmail.com>
2025-04-15 21:08:27 -0300

    feat: create notification entity for domain layer

---
+++ 4-ddd-nodejs/projeto-ddd-nodejs/src/domain/notification/enterprise/notification.ts
@@ -0,0 +1,50 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface NotificationProps {
+  // essa propriedade faz parte da regra do DDD
+  // de não nomear de forma generica e usar a linguagem do dominio
+  recipientId: UniqueEntityID
+  title: string
+  content: string
+  readAt?: Date
+  createdAt: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get readAt() {
+    return this.props.readAt
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
