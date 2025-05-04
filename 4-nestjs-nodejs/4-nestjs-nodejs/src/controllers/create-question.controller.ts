371faed3dfb214fa70c733085d7ec68d27b90494
felipy <felipy81br@gmail.com>
2025-05-03 21:57:27 -0300

    feat: add CurrentUser decorator and update CreateQuestionController to use it for user payload retrieval

--- 4-nestjs-nodejs/4-nestjs-nodejs/src/controllers/create-question.controller.ts
+++ 4-nestjs-nodejs/4-nestjs-nodejs/src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -8,7 +10,8 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
