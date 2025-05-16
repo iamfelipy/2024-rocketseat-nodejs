9a5224b53eb75dda947866e5b979c08fd1fe0229
felipy <felipy81br@gmail.com>
2025-05-16 09:43:41 -0300

    fix: correct accessors in Student class to use props for name, email, and password

--- 4-nestjs-nodejs/4-nestjs-nodejs/src/domain/forum/enterprise/entities/student.ts
+++ 4-nestjs-nodejs/4-nestjs-nodejs/src/domain/forum/enterprise/entities/student.ts
@@ -1,28 +1,28 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface StudentProps {
+export interface StudentProps {
   name: string
   email: string
   password: string
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
 
     return student
-  }
-
-  get name() {
-    return this.name
-  }
-
-  get email() {
-    return this.email
-  }
-
-  get password() {
-    return this.password
   }
 }
