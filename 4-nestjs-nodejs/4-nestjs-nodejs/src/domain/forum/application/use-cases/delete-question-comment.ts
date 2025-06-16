6d39ac5a0c89b273db927677d18a9cb0c63d970e
felipy <felipy81br@gmail.com>
2025-06-16 12:00:24 -0300

    refactor(domain/forum): remove unused import in delete-question-comment use case
    - Clean up the delete-question-comment.ts file by removing the unused Inject import from @nestjs/common, improving code clarity and maintainability.

--- 4-nestjs-nodejs/4-nestjs-nodejs/src/domain/forum/application/use-cases/delete-question-comment.ts
+++ 4-nestjs-nodejs/4-nestjs-nodejs/src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -2,7 +2,7 @@ import { Either, left, right } from '@/core/either'
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
-import { Inject, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteQuestionCommentUseCaseRequest {
   authorId: string
