c2a82dcb7ad2f9f1e58f44880dc87314a506cd84
Felipy <felipy81br@gmail.com>
2025-08-13 22:13:36 -0300

    refactor: update Shipment entity to use PhotoAttachment and simplify properties

--- 5-fast-feet-api/src/domain/core/enterprise/entities/shipment.ts
+++ 5-fast-feet-api/src/domain/core/enterprise/entities/shipment.ts
@@ -2,33 +2,21 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { ShipmentStatus } from '@/core/enums/shipment-status'
 import { Optional } from '@/core/types/optional'
+import { PhotoAttachment } from './photoAttachment'
 
 export interface ShipmentProps {
-  // Unique identifier for the shipment
   id: UniqueEntityID
-  // Status of the shipment (e.g., pending, delivered, returned)
   statusShipment: ShipmentStatus
-  // Unique identifier for the recipient
   recipientId: UniqueEntityID
-  // Delivery address
   deliveryAddress: string
-  // Latitude of the delivery address
   deliveryLatitude: number
-  // Longitude of the delivery address
   deliveryLongitude: number
-  // Pickup date
   pickupDate?: Date | null
-  // Delivery date
   deliveryDate?: Date | null
-  // Returned date
   returnedDate?: Date | null
-  // URL of the delivery photo
-  photoId?: UniqueEntityID | null
-  // Unique identifier for the assigned courier
+  photo?: PhotoAttachment | null
   assignedCourierId?: UniqueEntityID | null
-  // Creation date
   createdAt: Date
-  // Update date
   updatedAt?: Date | null
 }
 
@@ -49,6 +37,11 @@ export class Shipment extends Entity<ShipmentProps> {
 
   get deliveryAddress() {
     return this.props.deliveryAddress
+  }
+
+  set deliveryAddress(address: string) {
+    this.props.deliveryAddress = address
+    this.touch()
   }
 
   get deliveryLatitude() {
@@ -96,12 +89,12 @@ export class Shipment extends Entity<ShipmentProps> {
     this.touch()
   }
 
-  get photoId() {
-    return this.props.photoId
+  get photo() {
+    return this.props.photo
   }
 
-  set photoId(photoId) {
-    this.props.photoId = photoId
+  set photo(photo) {
+    this.props.photo = photo
     this.touch()
   }
 
@@ -126,29 +119,21 @@ export class Shipment extends Entity<ShipmentProps> {
     this.props.updatedAt = new Date()
   }
 
-  markAsDelivered(photoId: UniqueEntityID, courierId: UniqueEntityID) {
+  markAsDelivered(photo: PhotoAttachment, courierId: UniqueEntityID) {
     if (this.assignedCourierId !== courierId) {
       throw new Error('Você não pode entregar essa encomenda.')
     }
-    if (!photoId) {
+    if (!photo) {
       throw new Error('Foto obrigatória para entrega.')
     }
 
-    this.props.photoId = photoId
+    this.props.photo = photo
     this.props.statusShipment = ShipmentStatus.DELIVERED
     this.props.deliveryDate = new Date()
   }
 
   static create(
-    props: Optional<
-      ShipmentProps,
-      | 'pickupDate'
-      | 'deliveryDate'
-      | 'returnedDate'
-      | 'photoId'
-      | 'assignedCourierId'
-      | 'createdAt'
-    >,
+    props: Optional<ShipmentProps, 'createdAt'>,
     id?: UniqueEntityID,
   ) {
     const shipment = new Shipment(
