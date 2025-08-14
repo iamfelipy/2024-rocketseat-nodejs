3fac6658b30487ea2df3e7221323c4cce4e82d16
Felipy <felipy81br@gmail.com>
2025-08-13 21:53:57 -0300

    feat: add Admin entity

---
+++ 5-fast-feet-api/src/domain/core/enterprise/entities/admin.ts
@@ -0,0 +1,27 @@
+import { Optional } from '@/core/types/optional'
+import { User, UserProps } from './user'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { UserRole } from '@/core/enums/enum-user-role'
+
+export interface AdminProps extends UserProps {}
+
+export class Admin extends User<AdminProps> {
+  isAdmin() {
+    return this.props.roles.includes(UserRole.ADMIN)
+  }
+
+  static create(
+    props: Optional<AdminProps, 'createdAt' | 'roles'>,
+    id?: UniqueEntityID,
+  ) {
+    const admin = new Admin(
+      {
+        ...props,
+        roles: props.roles ?? [UserRole.ADMIN],
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+    return admin
+  }
+}
