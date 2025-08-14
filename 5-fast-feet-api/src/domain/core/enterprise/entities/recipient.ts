e4f40963e6f392b397ff9a39c68b4d2c5094e415
Felipy <felipy81br@gmail.com>
2025-08-13 21:54:11 -0300

    feat: add Recipient entity

---
+++ 5-fast-feet-api/src/domain/core/enterprise/entities/recipient.ts
@@ -0,0 +1,27 @@
+import { UserRole } from '@/core/enums/enum-user-role'
+import { User, UserProps } from './user'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface RecipientProps extends UserProps {}
+
+export class Recipient extends User<RecipientProps> {
+  isRecipient() {
+    return this.props.roles.includes(UserRole.RECIPIENT)
+  }
+
+  static create(
+    props: Optional<RecipientProps, 'createdAt' | 'roles'>,
+    id?: UniqueEntityID,
+  ) {
+    const recipient = new Recipient(
+      {
+        ...props,
+        roles: props.roles ?? [UserRole.RECIPIENT],
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+    return recipient
+  }
+}
