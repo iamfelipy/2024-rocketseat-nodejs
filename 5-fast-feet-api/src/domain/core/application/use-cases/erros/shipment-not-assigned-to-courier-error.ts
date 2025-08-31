2bd7b18338eb9b0dae8c00f6b850a2fb5cae417a
Felipy <felipy81br@gmail.com>
2025-08-31 18:37:12 -0300

    fix: update error message for ShipmentNotAssignedToCourierError to clarify the issue

--- 5-fast-feet-api/src/domain/core/application/use-cases/erros/shipment-not-assigned-to-courier-error.ts
+++ 5-fast-feet-api/src/domain/core/application/use-cases/erros/shipment-not-assigned-to-courier-error.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from "@/core/erros/use-case-error";
 
 export class ShipmentNotAssignedToCourierError extends Error implements UseCaseError {
   constructor() {
-    super("Shipment is not in the correct status for this operation.")
+    super("Shipment is not assigned to the courier.")
   }
 } 
