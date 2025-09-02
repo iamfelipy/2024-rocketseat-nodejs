a2e9408a9c02d455fd856c99ea733c328b50b936
Felipy <felipy81br@gmail.com>
2025-09-02 20:29:08 -0300

    feat: add Encrypter and HashComparer abstractions with fake implementations for testing

--- 5-fast-feet-api/src/test/cryptography/fake-hasher.ts
+++ 5-fast-feet-api/src/test/cryptography/fake-hasher.ts
@@ -4,4 +4,8 @@ export class FakeHasher implements HashGenerator {
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed')
   }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return plain.concat('-hashed') === hash
+  }
 }
