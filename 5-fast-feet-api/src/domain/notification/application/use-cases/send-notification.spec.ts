835209b050ed70d7a4c43d005988afebfd6cc506
Felipy <felipy81br@gmail.com>
2025-09-05 19:09:15 -0300

    feat, test: implement SendNotification use case with in-memory repository and corresponding tests

---
+++ 5-fast-feet-api/src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryNotificationsRepository } from "@/test/repositories/in-memory-notifications-repository"
+import { it, beforeEach, describe, expect } from "vitest"
+import { SendNotificationUseCase } from "./send-notification"
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: SendNotificationUseCase
+
+describe('Send notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sut = new SendNotificationUseCase(inMemoryNotificationsRepository)
+  })
+  it('should be able to send a notification', async () => {
+    const result = await sut.execute({
+      recipientId: '1',
+      title: 'Nova notificação',
+      content: 'Conteudo da notificação'
+    })
+    
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0]).toEqual(result.value?.notification)
+  })
+})
