10f71bb24fc3a82fbe582b400f5b52248290e898
Felipy <felipy81br@gmail.com>
2025-09-13 14:33:41 -0300

    feat: add PrismaService to manage database connections in the application

--- 5-nestjs-fast-feet-api/src/app.controller.ts
+++ 5-nestjs-fast-feet-api/src/app.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
-  index(): string {
-    return this.appService.getHello()
+  async index() {
+    return await this.prisma.user.findFirst()
   }
 
   @Post('/hello')
-  store(): string {
-    return 'teste'
+  async store() {
+    return await this.prisma.user.create({
+      data: {
+        name: 'sergio camelo',
+        cpf: '06450376988',
+        address: 'emilio dias fontes',
+        latitude: 12333443,
+        longitude: 221313123,
+      },
+    })
   }
 }
