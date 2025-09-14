26cddb072b1e78afa1ef52b4f1758331bd406b04
Felipy <felipy81br@gmail.com>
2025-09-14 14:05:23 -0300

    feat: enhance AuthenticateController to validate user credentials and return access token

--- 5-nestjs-fast-feet-api/src/controllers/authenticate.controller.ts
+++ 5-nestjs-fast-feet-api/src/controllers/authenticate.controller.ts
@@ -1,14 +1,55 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const authenticateBodySchema = z.object({
+  cpf: z.string(),
+  password: z.string(),
+})
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({ sub: 'user-id' })
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { cpf, password } = body
 
-    return token
+    const user = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('User credentials do not match.')
+    }
+
+    const isPasswordvalid = await compare(password, user.password)
+
+    if (!isPasswordvalid) {
+      throw new UnauthorizedException('User credentials do not match.')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return {
+      access_token: accessToken,
+    }
   }
 }
