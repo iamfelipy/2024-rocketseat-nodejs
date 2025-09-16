a9934edeaf9dbcfa5a064e8a2f7d85e4cf0f981b
Felipy <felipy81br@gmail.com>
2025-09-16 19:47:23 -0300

    feat: add end-to-end tests for account creation using supertest and Vitest

--- refazendo-nestjs-forum/src/controllers/create-account.controller.e2e-spec.ts
+++ refazendo-nestjs-forum/src/controllers/create-account.controller.e2e-spec.ts
@@ -1,4 +1,41 @@
-import { test, expect } from 'vitest'
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2)
+import { test, expect, describe, beforeAll } from 'vitest'
+import { Test } from '@nestjs/testing'
+import { AppModule } from '@/app.module'
+import { INestApplication } from '@nestjs/common'
+import { PrismaService } from '@/prisma/prisma.service'
+import request from 'supertest'
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456789',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
