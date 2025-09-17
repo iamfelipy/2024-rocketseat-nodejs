352325a592575fe03df0661fa67d619fb4a7075b
Felipy <felipy81br@gmail.com>
2025-09-17 20:09:29 -0300

    feat: add end-to-end tests for user authentication using supertest and Vitest

---
+++ 5-nestjs-fast-feet-api/src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import { beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        cpf: '12345678900',
+        address: '123 Main St',
+        latitude: 40.7128,
+        longitude: -74.006,
+        password: await hash('123456', 8),
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      cpf: '12345678900',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    })
+  })
+})
