ac87e0a4fa20347e5a1dc6519eab69c5cb509ade
Felipy <felipy81br@gmail.com>
2025-09-19 14:39:56 -0300

    feat: add Prisma repositories to databaseModule

---
+++ refazendo-nestjs-forum/src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
@@ -0,0 +1,26 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+
+export class PrismaAnswerCommentsRepository
+  implements AnswerCommentsRepository
+{
+  findById(id: string): Promise<AnswerComment | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  create(answerComment: AnswerComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(answerComment: AnswerComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
---
+++ refazendo-nestjs-forum/src/infra/database/prisma/repositories/prisma-answer-repository.ts
@@ -0,0 +1,28 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+
+export class PrismaAnswersRepository implements AnswersRepository {
+  findById(id: string): Promise<Answer | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<Answer[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  save(question: Answer): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(answer: Answer): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  create(answer: Answer): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
