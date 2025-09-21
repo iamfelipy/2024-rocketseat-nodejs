0475dde2b95b995a5dd41a724e58fbcf1cda5f9e
Felipy <felipy81br@gmail.com>
2025-09-21 17:06:56 -0300

    feat: add use-case authenticate-student

---
+++ refazendo-nestjs-forum/src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { StudentsRepository } from '../repositories/students-repository'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+import { Either, left, right } from '@/core/either'
+import { Encrypter } from '../cryptography/encrypter'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashCompare: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({ email, password }: AuthenticateUseCaseRequest) {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashCompare.compare(
+      password,
+      student.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
