6086c37fee2fb3cb742e091d1bfc39743280e3db
Felipy <felipy81br@gmail.com>
2025-09-22 19:36:53 -0300

    refactor: enhance error handling in controllers

--- refazendo-nestjs-forum/src/infra/http/controllers/create-question.controller.ts
+++ refazendo-nestjs-forum/src/infra/http/controllers/create-question.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { UserPayload } from '@/infra/auth/jwt.strategy'
@@ -28,11 +34,15 @@ export class CreateQuestionController {
     const { title, content } = body
     const userId = user.sub
 
-    await this.createQuestionUseCase.execute({
+    const result = await this.createQuestionUseCase.execute({
       authorId: userId,
       title,
       content,
       attachmentsIds: [],
     })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
   }
 }
