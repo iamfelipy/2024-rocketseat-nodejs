02e6cc721e78f4bbfbeacc5e535036fdcaee4ae3
felipy <felipy81br@gmail.com>
2025-09-30 20:11:37 -0300

    feat: implement CommentOnAnswerController and use case with E2E tests for commenting on answers

---
+++ refazendo-nestjs-forum/src/infra/http/controllers/comment-on-answer.controller.ts
@@ -0,0 +1,46 @@
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const commentOnAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnAnswerBodySchema)
+
+type CommentOnAnswerBodySchema = z.infer<typeof commentOnAnswerBodySchema>
+
+@Controller('/answers/:answerId/comments')
+export class CommentOnAnswerController {
+  constructor(private commentOnAnswer: CommentOnAnswerUseCase) {}
+
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('answerId') answerId: string,
+    @Body(bodyValidationPipe) body: CommentOnAnswerBodySchema,
+  ) {
+    const userId = user.sub
+
+    const { content } = body
+
+    const result = await this.commentOnAnswer.execute({
+      answerId,
+      authorId: userId,
+      content,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
