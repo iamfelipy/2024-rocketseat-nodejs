1a4b1790803e747a7f147792e0586cb76c5277f4
felipy <felipy81br@gmail.com>
2025-10-04 16:39:41 -0300

    feat(prisma): implement role-based filtering in findById and findByCPF methods of PrismaAdminsRepository

--- 5-nestjs-fast-feet-api/src/infra/database/prisma/repositories/prisma-admins-repository.ts
+++ 5-nestjs-fast-feet-api/src/infra/database/prisma/repositories/prisma-admins-repository.ts
@@ -3,19 +3,37 @@ import { Admin } from '@/domain/core/enterprise/entities/admin'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaAdminMapper } from '../mappers/prisma-admin-mapper'
+import { UserRole } from '@/core/enums/enum-user-role'
 
 @Injectable()
 export class PrismaAdminsRepository implements AdminsRepository {
   constructor(private prisma: PrismaService) {}
 
-  findById(id: string): Promise<Admin | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Admin | null> {
+    // Only fetch user if roles contains 'ADMIN'
+    const admin = await this.prisma.user.findUnique({
+      where: {
+        id,
+        roles: {
+          has: UserRole.ADMIN,
+        },
+      },
+    })
+
+    if (!admin) {
+      return null
+    }
+
+    return PrismaAdminMapper.toDomain(admin)
   }
 
   async findByCPF(cpf: string): Promise<Admin | null> {
     const admin = await this.prisma.user.findUnique({
       where: {
         cpf,
+        roles: {
+          has: UserRole.ADMIN,
+        },
       },
     })
 
