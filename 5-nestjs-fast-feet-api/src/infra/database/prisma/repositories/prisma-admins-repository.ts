75983d927b582192482efce427870a0744a45e12
felipy <felipy81br@gmail.com>
2025-10-05 20:36:31 -0300

    refactor(prisma): remove role-based filtering from findById and findByCPF methods in PrismaAdminsRepository

--- 5-nestjs-fast-feet-api/src/infra/database/prisma/repositories/prisma-admins-repository.ts
+++ 5-nestjs-fast-feet-api/src/infra/database/prisma/repositories/prisma-admins-repository.ts
@@ -3,20 +3,15 @@ import { Admin } from '@/domain/core/enterprise/entities/admin'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaAdminMapper } from '../mappers/prisma-admin-mapper'
-import { UserRole } from '@/core/enums/enum-user-role'
 
 @Injectable()
 export class PrismaAdminsRepository implements AdminsRepository {
   constructor(private prisma: PrismaService) {}
 
   async findById(id: string): Promise<Admin | null> {
-    // Only fetch user if roles contains 'ADMIN'
     const admin = await this.prisma.user.findUnique({
       where: {
         id,
-        roles: {
-          has: UserRole.ADMIN,
-        },
       },
     })
 
@@ -31,9 +26,6 @@ export class PrismaAdminsRepository implements AdminsRepository {
     const admin = await this.prisma.user.findUnique({
       where: {
         cpf,
-        roles: {
-          has: UserRole.ADMIN,
-        },
       },
     })
 
