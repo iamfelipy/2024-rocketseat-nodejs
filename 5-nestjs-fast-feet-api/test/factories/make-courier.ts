85c9bf9acc8e8427b54e2c7abdc0030642bcbb04
felipy <felipy81br@gmail.com>
2025-10-10 19:07:04 -0300

    feat(factory): add CourierFactory for Prisma integration

--- 5-nestjs-fast-feet-api/test/factories/make-courier.ts
+++ 5-nestjs-fast-feet-api/test/factories/make-courier.ts
@@ -1,23 +1,47 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Courier, CourierProps } from "@/domain/core/enterprise/entities/courier";
-import { Location } from "@/domain/core/enterprise/entities/value-objects/location";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Courier,
+  CourierProps,
+} from '@/domain/core/enterprise/entities/courier'
+import { Location } from '@/domain/core/enterprise/entities/value-objects/location'
+import { PrismaCourierMapper } from '@/infra/database/prisma/mappers/prisma-courier-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeCourier(
   override: Partial<CourierProps> = {},
-  id?: UniqueEntityID
+  id?: UniqueEntityID,
 ) {
-  const courier = Courier.create({
-    name: faker.person.fullName(),
-    cpf: faker.string.numeric(11),
-    password: faker.internet.password(),
-    location: Location.create({
-      address: faker.location.streetAddress(),
-      latitude: faker.location.latitude(),
-      longitude: faker.location.longitude(),
-    }),
-    ...override,
-  }, id)
+  const courier = Courier.create(
+    {
+      name: faker.person.fullName(),
+      cpf: faker.string.numeric(11),
+      password: faker.internet.password(),
+      location: Location.create({
+        address: faker.location.streetAddress(),
+        latitude: faker.location.latitude(),
+        longitude: faker.location.longitude(),
+      }),
+      ...override,
+    },
+    id,
+  )
 
   return courier
+}
+
+@Injectable()
+export class CourierFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaCourier(data: Partial<CourierProps> = {}): Promise<Courier> {
+    const courier = makeCourier(data)
+
+    await this.prisma.user.create({
+      data: PrismaCourierMapper.toPrisma(courier),
+    })
+
+    return courier
+  }
 }
